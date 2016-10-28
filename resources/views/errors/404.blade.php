ad8bee3a48efe154c10cc0c11b9e04d8c44d3bc5
banner <banner@dev>
2016-10-28 16:05:12 +0300

    Add 404 template

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+
+<style>
+    
+
+    body {
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        color: #B0BEC5;
+        font-weight: 100;
+        font-family: 'Arial';
+    }
+
+    .container {
+        text-align: center;
+    }
+
+    .content {
+        text-align: center;
+    }
+
+    .title {
+        font-size: 72px;
+        margin-bottom: 40px;
+    }
+</style>
+    
+<div class="container">
+    <div class="content">
+        <div class="title">Такой страницы не существует</div>
+    </div>
+</div>
+   
+
+@endsection
